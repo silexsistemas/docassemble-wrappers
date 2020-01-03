82ade2f5ddc7bfbc29d7aa62954db5d9c7f85842
rvnovaes <rvnovaes@gmail.com>
2020-01-02 22:54:47 -0300

    Changed package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='docassemble_wrappers',
-    version='0.0.1',
+    version='0.0.2',
     packages=['docassemble_wrappers'],
     url='https://github.com/silexsistemas/docassemble-wrappers',
     license='MIT',
