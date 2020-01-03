f308d5fd707043352fddc03ae1af54fdd07a0aad
rvnovaes <rvnovaes@gmail.com>
2020-01-02 22:33:09 -0300

    Added package info

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='docassemble_wrappers',
+    version='0.0.1',
+    packages=['docassemble_wrappers'],
+    url='https://github.com/silexsistemas/docassemble-wrappers',
+    license='MIT',
+    author='Roberto Vasconcelos Novaes',
+    author_email='roberto.novaes@silexsistemas.com.br',
+    description='Wrappers to external functionalities and Docassemble API'
+)
