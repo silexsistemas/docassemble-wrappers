7b7088fd7e78d9d498b9c9408e82fa80e29f4de6
rvnovaes <rvnovaes@gmail.com>
2020-01-02 22:34:52 -0300

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     license='MIT',
     author='Roberto Vasconcelos Novaes',
     author_email='roberto.novaes@silexsistemas.com.br',
-    description='Wrappers to external functionalities and Docassemble API'
+    description='Wrappers to external functionalities and Docassemble API',
+    install_requires=[
+        'validator-collection-br',
+    ],
 )
